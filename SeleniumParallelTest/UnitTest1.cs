83e8780bd3378d7f6ec8bcd96edad03e29d4130f
Jenny Pichardo <mlpichardo1@gmail.com>
2018-08-09 14:33:40 -0400

    minor edits

--- SeleniumParallelTest/UnitTest1.cs
+++ SeleniumParallelTest/UnitTest1.cs
@@ -8,6 +8,7 @@ namespace SeleniumParallelTest
     [Parallelizable]
     public class FireFoxTesting : Hooks
     {
+        
         public FireFoxTesting() : base(BrowserType.Chrome)
         {
         }
@@ -15,7 +16,7 @@ namespace SeleniumParallelTest
         [Test]
         public void FireFoxGoogleTest()
         {
-            Driver.Navigate().GoToUrl("www.google.com");
+            Driver.Navigate().GoToUrl("http://www.google.com");
             Driver.FindElement(By.Name("q")).SendKeys("Selenium");
             System.Threading.Thread.Sleep(10000);
             Driver.FindElement(By.Name("btnG")).Click();
@@ -35,7 +36,7 @@ namespace SeleniumParallelTest
         [Test]
         public void ChromeGoogleTest()
         {
-            Driver.Navigate().GoToUrl("www.google.com");
+            Driver.Navigate().GoToUrl("http://www.google.com");
             Driver.FindElement(By.Name("q")).SendKeys("ExecuteAutomation");
             Driver.FindElement(By.Name("btnG")).Click();
             Assert.That(Driver.PageSource.Contains("ExecuteAutomation"), Is.EqualTo(true),
